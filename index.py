1cd21fb0af5f9994c5d3446515ed27b3f08b051a
jeanJb <162447388+jeanJb@users.noreply.github.com>
2024-06-06 17:37:42 -0500

    feat:update readme.md

--- index.py
+++ index.py
@@ -28,6 +28,9 @@ def asistencia():
 def home():
     return render_template('home.html')
 
+@app.route('/index')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
       app.secret_key="jean"
